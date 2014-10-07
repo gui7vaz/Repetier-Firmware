25fd82209366d89745b93d6fbde06f19960416c7
Guilherme Vaz <gui7vaz@gmail.com>
2014-10-06 21:24:19 -0300

    Removendo configs Delta

--- src/ArduinoAVR/Repetier/Configuration.h
+++ src/ArduinoAVR/Repetier/Configuration.h
@@ -110,40 +110,6 @@ If a motor turns in the wrong direction change INVERT_X_DIR or INVERT_Y_DIR.
 // ##                               Calibration                                            ##
 // ##########################################################################################
 
-/** Drive settings for the Delta printers
-*/
-#if DRIVE_SYSTEM==3
-    // ***************************************************
-    // *** These parameter are only for Delta printers ***
-    // ***************************************************
-
-/** \brief Delta drive type: 0 - belts and pulleys, 1 - filament drive */
-#define DELTA_DRIVE_TYPE 0
-
-#if DELTA_DRIVE_TYPE == 0
-/** \brief Pitch in mm of drive belt. GT2 = 2mm */
-#define BELT_PITCH 2
-/** \brief Number of teeth on X, Y and Z tower pulleys */
-#define PULLEY_TEETH 20
-#define PULLEY_CIRCUMFERENCE (BELT_PITCH * PULLEY_TEETH)
-#elif DELTA_DRIVE_TYPE == 1
-/** \brief Filament pulley diameter in milimeters */
-#define PULLEY_DIAMETER 10
-#define PULLEY_CIRCUMFERENCE (PULLEY_DIAMETER * 3.1415927)
-#endif
-
-/** \brief Steps per rotation of stepper motor */
-#define STEPS_PER_ROTATION 200
-
-/** \brief Micro stepping rate of X, Y and Y tower stepper drivers */
-#define MICRO_STEPS 16
-
-// Calculations
-#define AXIS_STEPS_PER_MM ((float)(MICRO_STEPS * STEPS_PER_ROTATION) / PULLEY_CIRCUMFERENCE)
-#define XAXIS_STEPS_PER_MM AXIS_STEPS_PER_MM
-#define YAXIS_STEPS_PER_MM AXIS_STEPS_PER_MM
-#define ZAXIS_STEPS_PER_MM AXIS_STEPS_PER_MM
-#else
 // *******************************************************
 // *** These parameter are for all other printer types ***
 // *******************************************************
